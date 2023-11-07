5b81a475d08885905892c6480bebc127a6a72981
BenThere <benjaminbirdsall@icloud.com>
2023-11-06 17:10:57 -0700

    organize server.js

--- server.js
+++ server.js
@@ -1,15 +1,31 @@
 const express = require('express');
-const app = express();
-
+const mongoose = require('mongoose');
+const cors = require('cors');
 const userRoutes = require('./routes/userRoutes');
 const thoughtRoutes = require('./routes/thoughtRoutes');
 const reactionRoutes = require('./routes/reactionRoutes');
 
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+// MongoDB connection
+mongoose.connect('mongodb://localhost/socialnetworkdb', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+// Routes
 app.use('/api', userRoutes);
 app.use('/api', thoughtRoutes);
 app.use('/api', reactionRoutes);
 
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
